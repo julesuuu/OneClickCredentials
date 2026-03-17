a5f0b0cd3c1f67b08d3cc9ce2bb110b4427f5a5e
Julius De Perio <julesrdeperio@gmail.com>
2026-03-17 15:02:36 +0800

    feat: add social provider configuration for Google in auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,13 @@ const db = await getDatabase();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
