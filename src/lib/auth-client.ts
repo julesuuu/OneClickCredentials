60d72030c86142ee6d5ea1e1a0ef2fca6188dc9b
Julius De Perio <julesrdeperio@gmail.com>
2026-03-23 23:08:52 +0800

    feat(auth): add email OTP support for two-factor authentication

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient, twoFactorClient } from "better-auth/client/plugins";
+import { emailOTPClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
-  plugins: [twoFactorClient(), adminClient()],
+  plugins: [twoFactorClient(), adminClient(), emailOTPClient()],
 });
