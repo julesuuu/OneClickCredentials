10531b5df077c72eba4775663d5fb01a3773f158
Julius De Perio <julesrdeperio@gmail.com>
2026-04-23 02:05:09 +0800

    feat(uploadthing): add file upload router and UI components

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,17 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  }).onUploadComplete(async ({ file }) => {
+    console.log("file url", file.ufsUrl);
+    console.log(file);
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
